110a9959808df36f0ff18d37f5f98e127a7558fe
DanilaMaher <daniilnews@gmail.com>
2018-12-18 17:39:39 +0300

    File reader and writer implemented

---
+++ TestsGenerator/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class FileReader
+    {
+        public async Task<string> ReadFileAsync(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
---
+++ TestsGenerator/FileWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    class FileWriter
+    {
+        public async Task WriteFileAsync(List<GeneratedTestClassFile> generatedTests)
+        {    
+            foreach (var resultTest in generatedTests)
+            {
+                using (StreamWriter sw = new StreamWriter(resultTest.Name))
+                {
+                    await sw.WriteAsync(resultTest.Code);
+                }
+            }
+        }
+    }
+}
