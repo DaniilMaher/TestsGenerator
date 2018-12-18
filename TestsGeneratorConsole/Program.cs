a29d174474abc94ea28f8c2e81f2e9c6c71610ee
DanilaMaher <daniilnews@gmail.com>
2018-12-18 17:42:31 +0300

    Console app added

---
+++ TestsGeneratorConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using TestsGenerator;
+
+namespace ConsoleTestsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<string> paths = new List<string>();
+
+                paths.Add("e:\\Projects\\SPP\\TestsGenerator\\TestsGenerator\\FileReader.cs");
+                paths.Add("e:\\Projects\\SPP\\TestsGenerator\\TestsGenerator\\FileWriter.cs");
+                paths.Add("e:\\Projects\\SPP\\TestsGenerator\\TestsGenerator\\GeneratedTestClassFile.cs");
+                paths.Add("e:\\Projects\\SPP\\TestsGenerator\\TestsGenerator\\Generator.cs");                
+                string outputDirectory = "../../../GeneratedTests";
+                int numberOfFilesToRead = 5;
+                int numberOfFilesToWrite = 5;
+                int maxTasksAmmount = 5;
+                Generator generator = new Generator(numberOfFilesToRead, numberOfFilesToWrite, maxTasksAmmount, outputDirectory);
+                generator.Generate(paths).Wait();
+                Console.WriteLine("Files generated");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
